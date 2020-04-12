7c426c5ddbaa90ad4e22d3029fa4f8fb23c76129
Baha eddine chammakhi <bahachammakhi25@gmail.com>
2020-04-12 01:02:20 +0100

    CRUD employee

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyBTzUA6G8zHgmSVHmCs81dSczP0kayNCOI",
+  authDomain: "vue-fs-b1e98.firebaseapp.com",
+  databaseURL: "https://vue-fs-b1e98.firebaseio.com",
+  projectId: "vue-fs-b1e98",
+  storageBucket: "vue-fs-b1e98.appspot.com",
+  messagingSenderId: "930555477391",
+  appId: "1:930555477391:web:0b38351e0fe3199b429ffa"
+};
